55fe1eb367568139efe2624f8ff088e4383981e4
rudenko-vl <84930686+rudenko-vl@users.noreply.github.com>
2024-04-13 14:02:15 +0300

    middlware cors

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import {
   createPersonValidation,
 } from "./validations/validations.js";
 import checkAuth from "./utils/checkAuth.js";
+import { allowCors } from "./utils/allowCors .js";
 import * as UserController from "./controllers/UserController.js";
 import * as PersonController from "./controllers/PersonController.js";
 import * as VisitsController from "./controllers/VisitsController.js";
@@ -16,8 +17,8 @@ import * as VisitsController from "./controllers/VisitsController.js";
 //     "mongodb+srv://investvesko:psOaRrCJ813mnSYD@cluster0.qxpgtnc.mongodb.net/cafe_visits?retryWrites=true&w=majority"
 //   )
 
-  mongoose
-    .connect(process.env.MONGODB_URI)
+mongoose
+  .connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("DB is ok");
   })
@@ -27,6 +28,7 @@ import * as VisitsController from "./controllers/VisitsController.js";
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(allowCors);
 
 app.post("/auth/register", registerValidation, UserController.register);
 app.post("/auth/login", loginValidation, UserController.login);
@@ -37,7 +39,6 @@ app.get("/employes", PersonController.getAll);
 app.get("/employes/:id", PersonController.getOne);
 app.post(
   "/employes",
-
   checkAuth,
   createPersonValidation,
   PersonController.create
