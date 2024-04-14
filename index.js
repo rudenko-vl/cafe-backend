d3a7ed5de570ecfdc5b35c7278c371045127a514
rudenko-vl <84930686+rudenko-vl@users.noreply.github.com>
2024-04-14 08:35:10 +0300

    h

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import checkAuth from "./utils/checkAuth.js";
 import * as UserController from "./controllers/UserController.js";
 import * as PersonController from "./controllers/PersonController.js";
 import * as VisitsController from "./controllers/VisitsController.js";
-// import corsOption from "./utils/corsMiddlevar.js";
+import corsOption from "./utils/corsMiddlevar.js";
 
 // mongoose
 //   .connect(
@@ -28,7 +28,7 @@ mongoose
 const app = express();
 app.use(express.json());
 // app.use(cors());
-app.options("*", cors());
+app.options("*", cors(corsOption));
 
 app.post("/auth/register", registerValidation, UserController.register);
 app.post("/auth/login", loginValidation, UserController.login);
